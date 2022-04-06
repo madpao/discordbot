01e125b91eb40829232470f7f4d335c5493c7a3f
madpao <83626430+madpao@users.noreply.github.com>
2022-04-06 17:08:55 +0900

    Add files via upload

---
+++ discordbot.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='msg!') 
+bot.load_extension('dispander')
+
+@bot.event
+async def on_ready():
+    print('Bot is ready.')
+
+bot.run('OTYxMDkxNTExNzA4MTg0NjA3.Ykz8OA.ovVdc7CJCf-tjyn3f2xjHfK7o_Q') 
