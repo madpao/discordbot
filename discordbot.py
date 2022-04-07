0bb55a4acce7571ea45fb7789be06e3751160222
madpao <83626430+madpao@users.noreply.github.com>
2022-04-07 11:28:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,20 +1,6 @@
 import discord
 import os
-import keep_alive
 from discord.ext import commands
-
-from flask import Flask
-from threading import Thread
-
-app = Flask('')
-@app.route('/')
-def main():
-    return 'Bot is aLive!'
-def run():
-    app.run(host="0.0.0.0", port=8080)
-def keep_alive():
-    server = Thread(target=run)
-    server.start()
 
 bot = commands.Bot(command_prefix='msg!') 
 bot.load_extension('dispander')
@@ -23,5 +9,4 @@ bot.load_extension('dispander')
 async def on_ready():
     print('Bot is ready.')
 
-keep_alive.keep_alive()
 bot.run('OTYxMDkxNTExNzA4MTg0NjA3.Ykz8OA.ovVdc7CJCf-tjyn3f2xjHfK7o_Q') 
