fbd7db173c47d582fdeab695437c0fa98c228130
madpao <83626430+madpao@users.noreply.github.com>
2022-04-17 21:00:52 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,4 +9,5 @@ bot.load_extension('dispander')
 async def on_ready():
     print('Bot is ready.')
 
-bot.run('OTYxMDkxNTExNzA4MTg0NjA3.Ykz8OA.ovVdc7CJCf-tjyn3f2xjHfK7o_Q') 
+bot.run('TOKEN') 
+#TOKENには自分のTOKENを入力
